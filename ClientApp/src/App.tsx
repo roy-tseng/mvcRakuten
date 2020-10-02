8e187df2cfb09612a6387d3a5f242575b9b377d5
roy-tseng <roy@infolink.com.tw>
2020-10-02 19:19:57 +0800

    applied mvc config

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import {Airport} from './components/Airport/Airport';
 
 import './custom.css'
 
@@ -11,6 +12,9 @@ export default () => (
     <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
+         //if the routing name is the same as asp.net core controller routing name, asp.net controller will hanlde it.
+         // because in pipeline, spa handler is behind controller
+        <Route path='/airport' component={Airport} />
         <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
     </Layout>
 );
---
+++ ClientApp/src/components/Airport/Airport.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export const Airport = () =>{
+
+
+    return(
+
+        <>
+            The latest Airport
+        </>
+
+    )
+}
